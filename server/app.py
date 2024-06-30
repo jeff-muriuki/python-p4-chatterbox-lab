6a816c1602bb602bd232ea919c39bffd5d8dc250
jeff muriuki <jeff.muriuki@studet.moringaschool.com>
2024-06-30 13:41:19 +0300

    initial

--- server/app.py
+++ server/app.py
@@ -14,13 +14,63 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.route('/messages', methods= ['GET', 'POST'])
+def messages(): 
+    if request.method== 'GET':
+        msgs= [msg.to_dict() for msg in Message.query.order_by(Message.created_at.asc()).all() ]
+        return make_response(jsonify(msgs), 200)
+    
+    elif request.method== 'POST':
+        data= request.json
+        new_message= Message(
+            body=data.get('body'),
+            username=data.get('username')
+        )
+        db.session.add(new_message)
+        db.session.commit()
 
-@app.route('/messages/<int:id>')
+        message_dict= new_message.to_dict()
+        response= make_response(
+            jsonify(message_dict),
+            201
+        )
+        return response
+    
+
+@app.route('/messages/<int:id>', methods=['DELETE', 'PATCH'])
 def messages_by_id(id):
-    return ''
+    message= Message.query.filter(Message.id == id).first()
+
+    
+    if request.method== 'PATCH':    
+        data= request.json
+        for key, value in data.items():
+            setattr(message, key, value)
+
+        db.session.add(message)
+        db.session.commit()
+
+        message_dict= message.to_dict()
+        response= make_response(
+            jsonify(message_dict),
+            200
+        )                    
+        return response
+    
+    elif request.method== 'DELETE':
+        db.session.delete(message)
+        db.session.commit()
+
+        response_body= {
+            'delete-successful': True,
+            'message': 'Message deleted'
+        }
+        response= make_response(
+            response_body,
+            200
+        )
+        return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
